73c715287e56e32c3313a672d176b35ffd649dab
kreivesler <kreivesler@hotmail.com>
2024-12-18 14:14:05 -0300

    commit 19 	new file:   src/views/ContatoPage.vue

---
+++ src/views/ContatoPage.vue
@@ -0,0 +1,93 @@
+<script setup>
+
+</script>
+
+<template>
+  <span>Contato</span>
+
+  <ContainerComponent id="container3" display-type="flex" bg-color="black" flex-d="row">
+    <div class="cardFooter">
+      <span>Treinamentos:</span>
+      <nav>
+        <a
+          href="https://www.udemy.com/course/javascript-do-basico-ao-avancado-orientado-a-objetos/?referralCode=9F4A4286CA81D5381F16">JavaScript
+          Orientado a Objetos</a>
+        <a href="https://www.udemy.com/course/curso-de-informatica-essencial/?referralCode=EB7A53AB3FDC78443041">Informática
+          e Pacote Office</a>
+        <a href="https://www.udemy.com/course/microsoft-excel-intermediario/?referralCode=CE4301E4AC8E4D423FF2">Microsoft
+          Excel</a>
+      </nav>
+    </div>
+
+    <div class="cardFooter">
+      <span>Nossos Serviços:</span>
+      <nav>
+        <a href="/services">{{ conteudoServicos[0].titulo }}</a>
+        <a href="/services">{{ conteudoServicos[1].titulo }}</a>
+        <a href="/services">{{ conteudoServicos[2].titulo }}</a>
+        <a href="/services">{{ conteudoServicos[3].titulo }}</a>
+      </nav>
+    </div>
+
+    <div class="cardFooter">
+      <span>Sobre a KR Riley Soluções:</span>
+      <nav>
+        <a href="/sobre">Quem somos</a>
+        <a href="/politica/privacidade">Política de privacidade</a>
+        <a href="/politica/servicos">Política de serviços</a>
+      </nav>
+    </div>
+
+    <div class="cardFooter">
+      <span>Redes Sociais:</span>
+      <ButtonComponent path="https://www.facebook.com/people/Kr-Riley-Solu%C3%A7%C3%B5es/61552679426384/"
+        type-button="button" msg="Facebook" bg-color="transparent" text-color="white" />
+      <ButtonComponent path="https://www.instagram.com/rileysolucoes/" type-button="button" msg="Instagram"
+        bg-color="transparent" text-color="white" />
+      <ButtonComponent path="https://www.linkedin.com/company/rileysolucoes/" type-button="button" msg="Linkedin"
+        bg-color="transparent" text-color="white" />
+    </div>
+
+
+  </ContainerComponent>
+</template>
+
+<style scoped>
+@media only screen and (min-width: 300px) {
+  .cardFooter {
+    display: flex;
+    flex-direction: column;
+    height: 80%;
+  }
+
+  .cardFooter span {
+    font-size: 1.2rem;
+  }
+
+  .cardFooter nav {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 2px;
+  }
+
+  .cardFooter a {
+    text-decoration: none;
+    color: white;
+  }
+}
+
+@media only screen and (min-width: 400px) {}
+
+@media only screen and (min-width: 600px) {}
+
+@media only screen and (min-width: 768px) {}
+
+@media only screen and (min-width: 992px) {}
+
+@media only screen and (min-width: 1200px) {
+
+  #container3 {
+    height: 300px !important;
+  }
+}
+</style>
