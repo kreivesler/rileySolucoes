fbc71865791d3a09a9a699791b36630e51d4af03
kreivesler <kreivesler@hotmail.com>
2024-12-18 16:25:03 -0300

    commit 21 	new file:   src/components/FormComponent.vue 	modified:   src/views/ContatoPage.vue

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,37 @@
+<scrip setup>
+
+</scrip>
+
+<template>
+<form>
+  <label for="">Nome da empresa:</label>
+  <input type="text" required >
+
+  <label for="">CNPJ:</label>
+  <input type="text" required >
+
+  <label for="">Nome do responsável:</label>
+  <input type="text" name="" id="" required >
+
+  <label for="">Informe seu email:</label>
+  <input type="email" required>
+
+  <label for="">Informe seu celular:</label>
+  <input type="tel" id="" pattern="[0-9]{3}-[0-9]{1}-[0-9]{4}-[0-9]{4}" required >
+
+  <label for="services">Selecione o serviço:</label>
+  <input list="listaServicos" id="services" type="text">
+  <datalist id="listaServicos">
+    <option value="Site Institucional"></option>
+    <option value="Landing Page"></option>
+    <option value="Otimização de Site"></option>
+    <option value="Consultoria e Suporte"></option>
+    <option value="Outros"></option>
+  </datalist>
+
+</form>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/ContatoPage.vue
+++ src/views/ContatoPage.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import ContainerComponent from '@/components/ContainerComponent.vue';
+import { conteudoServicos } from '@/data/texts';
 </script>
 
 <template>
