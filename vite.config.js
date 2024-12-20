8c51e5f811febce440ce85774297010f542e684c
Kreivesler Riley da Silva Luciano <54329663+kreivesler@users.noreply.github.com>
2024-12-19 23:04:25 -0300

    commit 30

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rileySolucoes/',
   plugins: [
     vue(),
     vueDevTools(),
