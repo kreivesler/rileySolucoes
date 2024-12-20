66b09e249ef088d82fa585b63f60f0f0e024e4e3
kreivesler <kreivesler@hotmail.com>
2024-12-20 01:03:29 -0300

    commit 42 	modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import PolicyService  from '../views/PolicyServicePage.vue'
 import Contato from '@/views/ContatoPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/rileySolucoes/'),
   routes: [
     {
       path: '/',
