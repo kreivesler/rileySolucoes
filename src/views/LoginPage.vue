943045d06d11fa4cb78c4a06f65a5023e9926b85
kreivesler <kreivesler@hotmail.com>
2024-12-28 09:24:50 -0300

    commit 56 	modified:   src/views/LoginPage.vue

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,7 +1,11 @@
 <script setup>
+import ContainerComponent from '@/components/ContainerComponent.vue';
+import CardComponent from '@/components/CardComponent.vue';
 </script>
 <template>
-<h1>Login page</h1>
+  <ContainerComponent>
+    <CardComponent titulo-card="Faça login" font-sizespan="2rem" font-weightspan="800" paragrafo-card="Preencha o formulário para ter acesso ao curso adquirido." />
+  </ContainerComponent>
 </template>
 <style scoped>
 </style>
