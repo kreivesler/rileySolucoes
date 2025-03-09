257406f8cd9c024a9da0d35762361dacd283fb9e
kreivesler <rileykreive@gmail.com>
2025-03-09 14:49:06 -0300

    commit 119 	modified:   src/views/DashboardPage.vue

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -5,13 +5,13 @@ import GridItem from '@/components/GridItem.vue';
 </script>
 <template>
   <ContainerComponent display-type="grid">
-    <GridItem font-sizep="0.8rem" -titulo="Curso de Informática Essencial e Pacote Office" -paragrafo="Aprenda informática essencial e o Pacote Office (Word, Excel, PowerPoint e Outlook). Ideal para melhorar sua produtividade e se destacar no mercado.">
+    <GridItem font-sizep="0.7rem" -titulo="Curso de Informática Essencial e Pacote Office" -paragrafo="Aprenda informática essencial e o Pacote Office (Word, Excel, PowerPoint e Outlook). Ideal para melhorar sua produtividade e se destacar no mercado.">
       <button>Começar</button>
     </GridItem>
-    <GridItem font-sizep="0.8rem" -titulo="Curso de JavaScript Orientado a Objetos" -paragrafo="Aprenda JavaScript orientado a objetos: classes, herança, encapsulamento e polimorfismo. Escreva códigos mais organizados e reutilizáveis.">
+    <GridItem font-sizep="0.7rem" -titulo="Curso de JavaScript Orientado a Objetos" -paragrafo="Aprenda JavaScript orientado a objetos: classes, herança, encapsulamento e polimorfismo. Escreva códigos mais organizados e reutilizáveis.">
       <button>Começar</button>
     </GridItem>
-    <GridItem font-sizep="0.8rem" -titulo="Curso de Microsoft Excel" -paragrafo="Domine o Microsoft Excel! Aprenda desde funções básicas até fórmulas avançadas, tabelas dinâmicas e gráficos para otimizar sua produtividade.">
+    <GridItem font-sizep="0.7rem" -titulo="Curso de Microsoft Excel" -paragrafo="Domine o Microsoft Excel! Aprenda desde funções básicas até fórmulas avançadas, tabelas dinâmicas e gráficos para otimizar sua produtividade.">
       <button>Começar</button>
     </GridItem>
   </ContainerComponent>
